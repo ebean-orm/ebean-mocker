a61c0378f5c64947771b2e8f6552317b36a014f3
rbygrave <robin.bygrave@gmail.com>
2014-07-10 20:47:03 +1200

    Add LoggedSql feature for capturing all the generated sql

---
+++ src/test/java/org/example/MyBean.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import com.avaje.ebean.Model;
+
+@Entity
+public class MyBean extends Model {
+
+  @Id 
+  Long id;
+  
+  String name;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+}
